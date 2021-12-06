6f0919465aca2827fca4cda1b28dd4b4e53c101c
Bozhidar Andonov <B.P.Andonov-1@student.tudelft.nl>
2021-12-06 07:45:49 +0100

    Add solution for day 6

---
+++ src/AoC_2021/Day6.java
@@ -0,0 +1,69 @@
+package AoC_2021;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day6 {
+    private static File file = new File("src/AoC_2021/input/day_6/big.txt");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    public static int part1() throws FileNotFoundException {
+        Scanner sc = new Scanner(file);
+        int[] fishDays = new int[9];
+
+        for(int i = 0; i < 9; i++) {
+            fishDays[i] = 0;
+        }
+
+        List<Integer> fish =
+                Arrays.stream(sc.nextLine().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+        for(var f : fish) {
+            fishDays[f]++;
+        }
+
+        for(int i = 0; i < 80; i++) {
+            int temp = fishDays[0];
+            for(int j = 1; j < 9; j++) {
+                fishDays[j - 1] = fishDays[j];
+            }
+            fishDays[6] += temp;
+            fishDays[8] = temp;
+        }
+
+        return Arrays.stream(fishDays).sum();
+    }
+
+    public static long part2() throws FileNotFoundException {
+        Scanner sc = new Scanner(file);
+        long[] fishDays = new long[9];
+
+        for(int i = 0; i < 9; i++) {
+            fishDays[i] = 0L;
+        }
+
+        List<Integer> fish =
+                Arrays.stream(sc.nextLine().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+        for(var f : fish) {
+            fishDays[f]++;
+        }
+
+        for(int i = 0; i < 256; i++) {
+            long temp = fishDays[0];
+            for(int j = 1; j < 9; j++) {
+                fishDays[j - 1] = fishDays[j];
+            }
+            fishDays[6] += temp;
+            fishDays[8] = temp;
+        }
+
+        return Arrays.stream(fishDays).sum();
+    }
+}
